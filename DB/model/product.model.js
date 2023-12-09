6aba89bd590222df1cf791d0625ad65ef430752c
sondosammar3 <sondosammar18@gmail.com>
2023-12-09 22:19:39 +0200

    pagination

--- DB/model/product.model.js
+++ DB/model/product.model.js
@@ -1,81 +1,81 @@
-import mongoose,{Schema,Types,model} from "mongoose";
+import mongoose, { Schema, Types, model } from "mongoose";
 
-const productSchema=new Schema({
-    name:{
-        type:String,
-        required:true,
-        min:4,
-        max:20,
+const productSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        min: 4,
+        max: 20,
     },
-    slug:{
-type:String,
-required:true,
+    slug: {
+        type: String,
+        required: true,
     },
-    description:{
-type:String,
-required:true,
+    description: {
+        type: String,
+        required: true,
     },
-    stock:{
-  type:Number,
-  default:1,
+    stock: {
+        type: Number,
+        default: 1,
     },
-    price:{
-     type:Number,
-     required:true,
+    price: {
+        type: Number,
+        required: true,
     },
-    discount:{
-          type:Number,
-          default:0,
+    discount: {
+        type: Number,
+        default: 0,
     },
-    finalPrice:{
-        type:Number,
+    finalPrice: {
+        type: Number,
     },
-    number_sellers:{
-        type:Number,
-        default:0,
+    number_sellers: {
+        type: Number,
+        default: 0,
     },
-    mainImage:{
-     type:Object,
-     required:true,
+    mainImage: {
+        type: Object,
+        required: true,
     },
-    subImages:[{
-         type:Object,
-         required:true,
+    subImages: [{
+        type: Object,
+        required: true,
     }],
-   
-    status:{
-        type:String,
-        default:'Active',
-        enum:['Active','Inactive'],
+
+    status: {
+        type: String,
+        default: 'Active',
+        enum: ['Active', 'Inactive'],
 
     },
-    isDeleted:{
-        type:Boolean,
-        default:false,
+    isDeleted: {
+        type: Boolean,
+        default: false,
     },
-    categoryId:{
-        type:Types.ObjectId,
-        ref:'Category',
-        required:true,
-     },
-     subcategoryId:{
-        type:Types.ObjectId,
-        ref:'Subcategory',
-        required:true,
-     },
-  
-    createdBy:{
-  type:Types.ObjectId,ref:'User',required:true,
+    categoryId: {
+        type: Types.ObjectId,
+        ref: 'Category',
+        required: true,
     },
-    updatedBy:{
-        type:Types.ObjectId,ref:'User',required:true,
+    subcategoryId: {
+        type: Types.ObjectId,
+        ref: 'Subcategory',
+        required: true,
+    },
+
+    createdBy: {
+        type: Types.ObjectId, ref: 'User', required: true,
+    },
+    updatedBy: {
+        type: Types.ObjectId, ref: 'User', required: true,
     }
 
-},{
-    timestamps:true,
-   
+}, {
+    timestamps: true,
+
 });
 
-const productModel=mongoose.models.Product || model('Product',productSchema);
+const productModel = mongoose.models.Product || model('Product', productSchema);
 
-  export default productModel
+export default productModel
--- src/modules/product/product.controller.js
+++ src/modules/product/product.controller.js
@@ -3,10 +3,22 @@ import categoryModel from "../../../DB/model/category.model.js";
 import subcategoryModel from "../../../DB/model/subcategory.model.js";
 import cloudinary from "../../services/cloudinary.js";
 import productModel from "../../../DB/model/product.model.js";
+import { pagination } from "../../services/pagination.js";
 
 export const getProducts =async (req, res) => {
-   const product=await productModel.find()
-   return res.json(product)
+  const {skip,limit}=pagination(req.query.page,req.query.limit)
+  const queryobj={...req.query}
+  const execquery=['page','limit','skip','sort']
+  execquery.map((ele)=>{
+    delete queryobj[ele]
+  })
+  queryobj=JSON.stringify(queryobj)
+  queryobj=queryobj.replace(/\b(gt|gte|lt|lte|in|nin|eq|niq|gte)\b /g,match=>`$${match}`)
+  const mongooseQuery=productModel.find(queryobj).limit(limit).skip(skip)
+  queryobj=JSON.parse(queryobj)
+  const product=await mongooseQuery.sort(req.query.sort?.replaceAll(',',' '))
+      const count =await productModel.estimatedDocumentCount() 
+  return res.json({message:"message",page:product.length,total:count,product})
 }
 
 export const createProduct = async (req, res) => {
@@ -37,4 +49,9 @@ export const createProduct = async (req, res) => {
         return res.status(400).json({ message: "Product not created" });
     }
     return res.status(201).json({ message: "succrss", product });
+}
+
+export const getProductsWithCategory=async(req,res)=>{
+    const product=await productModel.find({categoryId:req.params.categoryId})
+    return res.status(200).json({message:"success",product})  
 }
