79e90292cc81cafe958877d5f63c4faccb184781
sondosammar3 <sondosammar18@gmail.com>
2023-12-16 03:23:04 +0200

    display array of object (pdf)

--- src/services/pdf.js
+++ src/services/pdf.js
@@ -5,14 +5,27 @@ import { dirname, join } from "path";
 import { options } from "./option.js";
 const __filename=fileURLToPath(import.meta.url)
  const __dirname=dirname(__filename)
-
+ let users = [
+    {
+      name: "Shyam",
+      age: "26",
+    },
+    {
+      name: "Navjot",
+      age: "26",
+    },
+    {
+      name: "Vitthal",
+      age: "26",
+    },
+  ];
 export  const createPdf =()=>{
     //let html = fs.readFileSync("pdf.html", "utf8");
    const htmlpath=join(__dirname,'../../templtes/pdf.html')
    const html=fs.readFileSync(htmlpath,"utf8")
    var document = {
     html: html,
-    data: {},
+    data: {users},
     path: "./output.pdf",
    
   };
